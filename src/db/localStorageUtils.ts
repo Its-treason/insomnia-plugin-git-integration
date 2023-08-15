6cd52b0ba7c4d16ff3d2abe9f96b7976e84b2aca
Its-treason <39559178+Its-treason@users.noreply.github.com>
2023-08-15 23:48:44 +0200

    fix: Reading of current select project and workspace

--- src/db/localStorageUtils.ts
+++ src/db/localStorageUtils.ts
@@ -1,23 +1,9 @@
-function superTrim(char: string, target: string): string {
-  const pattern = new RegExp('^' + char + '+|' + char + '+$', 'g');
-  const newStr = target.replace(pattern, '');
-  return newStr;
-}
-
 export function getActiveProjectId(): string | null {
-  const projectId = window.localStorage.getItem('insomnia::meta::activeProjectId');
-  if (!projectId) {
-    return null;
-  }
-
-  return superTrim('"', projectId);
+  const projectId = localStorage.getItem('locationHistoryEntry').split('/')[4];
+  return projectId || null;
 }
 
 export function getActiveWorkspace(): string | null {
-  const workspaceId = window.localStorage.getItem('insomnia::meta::activeWorkspaceId');
-  if (!workspaceId) {
-    return null;
-  }
-
-  return superTrim('"', workspaceId);
+  const projectId = localStorage.getItem('locationHistoryEntry').split('/')[6];
+  return projectId || null;
 }
